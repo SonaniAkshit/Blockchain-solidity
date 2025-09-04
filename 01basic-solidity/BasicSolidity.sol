d4f9b7ee1cd30bd2c7cd13a91c026f2b2eb563ed
SonaniAkshit <sonaniakshit684@gmail.com>
2025-09-04 12:59:03 +0530

    solidity examples

---
+++ 01basic-solidity/BasicSolidity.sol
@@ -0,0 +1,18 @@
+// I'm a comment!
+// SPDX-License-Identifier: MIT
+
+
+pragma solidity 0.8.30;
+
+contract SimpleStorage {
+
+    // boolean, uint, int, address, bytes
+
+    bool FavoriteNumber = true;
+    uint32 favoriteNumber = 5;
+    string favoriteNumberInText = "Five";
+    int256 favoriteInt = -5;
+    address myAddress = 0x1066618d0973e44EfD2Fe5114fD18b64c6420AbB;
+    bytes32 favoriteBytes = "cat";
+
+}
